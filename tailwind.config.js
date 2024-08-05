c07fe999e16f0dabae5d43eee09af09eb7f82977
vparul <verma.parul14799@gmail.com>
2024-08-05 20:53:01 +0530

    extended color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light_gray_cyan_500: "hsl(180, 52%, 96%)",
+        light_gray_cyan_900: " hsl(180, 31%, 95%)",
+        dark_gray_cyan_500: "hsl(180, 8%, 52%)",
+        dark_gray_cyan_900: "hsl(180, 14%, 20%)",
+      }
+    },
   },
   plugins: [],
 }
